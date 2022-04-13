846f31415a73c89d9b97ef555ab35d62a60e5f46
suratinkru <toey0908toey@gmail.com>
2022-04-13 12:29:14 +0700

    ss

--- index.js
+++ index.js
@@ -35,10 +35,27 @@ const ifNotLoggedin = (req, res, next) => {
 }
 const ifLoggedin = (req,res,next) => {
     if(req.session.isLoggedIn){
-        return res.redirect('/home');
+        return res.redirect('/dashboard');
     }
     next();
 }
+
+app.get('/dashboard',(req,res)=>{
+    //session destroy
+
+    if(req.session.isLoggedIn){
+        dbConnection.execute("SELECT `username` FROM `users` WHERE `id`=?",[req.session.userID])
+    .then(([rows]) => {
+        res.render('dashboard',{
+            username:rows[0].username
+        });
+    });
+    
+}else{
+    return res.redirect('/backend');
+}
+
+});
 // END OF CUSTOM MIDDLEWARE
 // ROOT PAGE
 app.get('/', ifNotLoggedin, (req,res,next) => {
@@ -172,6 +189,104 @@ app.get('/about',(req,res)=>{
     req.session = null;
     res.render('about');
 });
+
+app.post('/signup',(req,res)=>{
+    //session destroy
+    console.log(req.body.password1);
+    let user = {
+        "username": req.body.email,
+        "password": req.body.password1
+}
+
+    // req.session = null;
+    bcrypt.hash(req.body.password1, 12).then((hash_pass) => {
+        console.log(hash_pass);
+        // INSERTING USER INTO DATABASE
+        dbConnection.execute("INSERT INTO `users`(`username`,`password`) VALUES(?,?)",[req.body.email ,hash_pass])
+        .then(result => {
+            res.send(`your account has been created successfully, Now you can <a href="/backend">Login</a>`);
+        }).catch(err => {
+            // THROW INSERTING USER ERROR'S
+            console.log("err:",err);
+            if (err)   res.render('signup');;
+        });
+    })
+    .catch(err => {
+        // THROW HASING ERROR'S
+        console.log(err);
+        if (err)   res.render('signup');;
+    })
+    // res.render('signup');
+});
+
+app.get('/signup',(req,res)=>{
+    //session destroy
+    req.session = null;
+    res.render('signup');
+});
+
+app.get('/backend',(req,res)=>{
+    //session destroy
+    req.session = null;
+    res.render('backend');
+});
+
+
+
+app.post('/signIn',ifLoggedin, [
+    body('user_email').custom((value) => {
+        return dbConnection.execute('SELECT username FROM users WHERE username=?', [value])
+        .then(([rows]) => {
+            if(rows.length == 1){
+                return true;
+                
+            }
+            return Promise.reject('Invalid username!');
+            
+        });
+    }),
+    body('user_pass','Password is empty!').trim().not().isEmpty(),
+], (req, res) => {
+    const validation_result = validationResult(req);
+    const {user_pass, user_email} = req.body;
+    console.log("user_pass, user_email:",user_pass, user_email);
+    if(validation_result.isEmpty()){
+        console.log("user_pass, sssssssuser_email:",user_pass, user_email);
+        dbConnection.execute("SELECT * FROM `users` WHERE `username`=?",[user_email])
+        .then(([rows]) => {
+            bcrypt.compare(user_pass, rows[0].password).then(compare_result => {
+                if(compare_result === true){
+                    req.session.isLoggedIn = true;
+                    req.session.userID = rows[0].id;
+                 
+                    res.redirect('/dashboard');
+                }
+                else{
+                    res.redirect('/backend');
+                }
+            })
+            .catch(err => {
+                if (err) throw err;
+            });
+
+
+        }).catch(err => {
+            if (err) throw err;
+        });
+    }
+    else{
+        console.log("user_pass, useddddddddr_email:");
+        let allErrors = validation_result.errors.map((error) => {
+            return error.msg;
+        });
+        // REDERING main PAGE WITH LOGIN VALIDATION ERRORS
+        res.redirect('/backend');
+    }
+});
+// END OF LOGIN PAGE
+
+
+
 
 app.use('/', (req,res) => {
     res.status(404).send('<h1>404 Page Not Found!</h1>');
